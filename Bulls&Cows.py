0fabe6827232ad0e30fb78d7dc249eb090fdf0f3
Tomáš Plechatý <tomplechaty@seznam.cz>
2024-08-19 14:24:13 +0200

    Snížení počtu funkcí, přidaná dokumentace funkcím, zjednodušení 2 funkcí, korekce názvu funkce

--- Bulls&Cows.py
+++ Bulls&Cows.py
@@ -8,15 +8,10 @@ discord: Gulas_D_Kom#9800
 import time
 import random
 
-def start_timer():
-    start = time.time()
-    return start
 
-def end_timer():
-    end = time.time()
-    return end    
     
 def intro():
+    """Vypíše úvodní pozdrav a vyzve ke hře"""
     print("Hi There!")
     print(47*"-")
     print("I've generated a random 4 digit number for you.")
@@ -24,28 +19,33 @@ def intro():
     print(47*"-")
 
 def generate_random_number():
-    first = random.randint(1,9)
-    second = random.randint(0,9)
-    while second == first:
-        second = random.randint(0,9)
-    third = random.randint(0,9)
-    while third == first or third == second:
-        third = random.randint(0,9)
-    fourth = random.randint(0,9)
-    while fourth == first or fourth == second or fourth == third:
-        fourth = random.randint(0,9)
-    first_str = str(first)
-    second_str = str(second)
-    third_str = str(third)
-    fourth_str = str(fourth)
-    random_number = first_str+second_str+third_str+fourth_str
+    """Vytvoří náhodné číslo s unikátními číslicemi nezačínající na nulu"""
+    first,second,third,fourth = None, None, None, None
+    #reset obsahu proměnných před novou generací čísla
+    pool = {1,2,3,4,5,6,7,8,9}
+    first = str(random.choice(list(pool)))
+    #print(first)
+    pool.add(0)
+    pool.remove(int(first))
+    #print(pool)
+    second = str(random.choice(list(pool)))
+    pool.remove(int(second))
+    #print(second)
+    #print(pool)
+    third = str(random.choice(list(pool)))
+    pool.remove(int(third))
+    #print(third)
+    #print(pool)
+    fourth = str(random.choice(list(pool)))
+    pool.remove(int(fourth))
+    #print(fourth)
+    #print(pool)
+    random_number = first+second+third+fourth
     return random_number
-    #V případě generace delšího unikátního čísla bych vybíral náhodné prvky z  množiny čísel 0-9 a vybrané prvky pak odebíral z množiny. 
-
-
-
     
-def input_control(string_of_numbers):
+def input_control(string_of_numbers:str):
+    """Kontrola, zda číslo na vstupu neobsahuje jiný počet cifer než 4,
+    zda neobsahuje nečíselné znaky, zda nezačíná na 0 , či zda neobsahuje duplicity"""
     first_char = string_of_numbers[0]
         
     if len(string_of_numbers) != 4:
@@ -57,26 +57,17 @@ def input_control(string_of_numbers):
         return False
     
     elif not string_of_numbers.isnumeric():
-        print("Číslo obsahuje nečíselné znaky")    
+        print("Číslo nesmí obshaovat nečíselné znaky")    
         return False
         
-    elif find_repeating_digits(string_of_numbers):
+    elif len(set(string_of_numbers)) != len(string_of_numbers):
         print("Číslo nesmí obsahovat duplicity")
         return False
 
     elif string_of_numbers.isdigit() and first_char != "0" and len(string_of_numbers) == 4:
-        return True
-        
-def find_repeating_digits(num):
+        return True 
     
-    str_num = str(num)  
-    if len(set(str_num)) != len(str_num):
-        print("duplicita")
-        return True
-    else:
-        return False    
-    
-def count_Bulls_and_Cows(guess,random_number):
+def count_bulls_and_cows(guess,random_number):
     
     bulls = 0
     cows = 0
@@ -91,40 +82,16 @@ def count_Bulls_and_Cows(guess,random_number):
             #print("cow pridana")                               #pro debug
     return bulls, cows
 
-
-def set_counter():
-    attempt = 0
-    return attempt
-
-def add_counter(attempt):
-    attempt+=1
-    return attempt
-
 def evaluation(bulls,cows):
-    if bulls == 1 and cows == 1:
-        print(bulls,"bull, ", cows, "cow")
-        return 
-    if bulls == 1 and cows != 1:
-        print(bulls,"bull, ", cows, "cows")
-        return 
-    if bulls != 1 and cows == 1:
-        print(bulls,"bulls, ", cows, "cow")
-        return     
-    if bulls != 1 and cows != 1: 
-        print(bulls,"bulls, ", cows, "cows")
-        return 
-    
-def get_stats():
-    pass
-
-def save_stats():
-    time = end - start
-    pass
-
-def calculate():
-    time = end-start
-    return time
-    pass
+    """Vypíše počet Býků a Krav ve správném tvaru jednotného nebo množného čísla"""
+    plural1 = ""
+    plural2 = ""
+    if bulls != 1:
+        plural1 = "s"
+    if cows != 1:
+        plural2 = "s"
+    print(bulls," bull",plural1," ", cows, " cow",plural2, sep="")
+    return 
 
 def guess_call():
     print("Enter a number:")
@@ -133,36 +100,33 @@ def guess_call():
     return guess
 
 def gratulation_stats_and_end(attempt):
+    """Oznámení vítězství, výpis počtu potřebných pokusů a spotřebovaný čas."""
     attempt+=1
     print("Correct, you've guessed the right number")
     print("in ",attempt, "guesses!")
     print(47*"-")
-    get_stats()
-    time = calculate()
+    time = end - start
     print("Your time was ",time//360,"h ", (time%360)//60,"min ", time%60,"sec"  )
-    save_stats()
-
-
-
+    
 
 
 intro()
 random_number = generate_random_number()
 #print (random_number) #pro kontrolu
-start = start_timer()
+start = time.time() #spustí stopky
 guess = guess_call()
-attempt = set_counter()
+attempt = 0 # nastaví počítadlo pokusů
 
 while guess != random_number:
     if not input_control(guess):
         guess = guess_call()
         continue
-    bulls, cows = count_Bulls_and_Cows(guess,random_number)
+    bulls, cows = count_bulls_and_cows(guess,random_number)
     evaluation(bulls,cows)
-    attempt = add_counter(attempt)
+    attempt +=1 # přičte pokus
     guess = guess_call()
         
-end = end_timer()
+end = time.time() #zastaví stopky
 gratulation_stats_and_end(attempt)
 
 
